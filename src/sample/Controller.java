03c82dd9bcf90be7c75e118e57cd80ec96aa4ee4
qoodboop <a.diarr.agla@gmail.com>
2018-11-28 18:31:34 +0100

    transfert des données via le port serie

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,72 +3,89 @@ import com.fazecast.jSerialComm.*  ;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 
 public class Controller {
 
+    float hum ;
+    float temp ;
+
     public void test(){
-                SerialPort comPort = SerialPort.getCommPorts()[0];
-                InputStream in = comPort.getInputStream();
+                        SerialPort comPort = SerialPort.getCommPorts()[0];
+                        InputStream in = comPort.getInputStream();
+                        SerialPort ports[] = SerialPort.getCommPorts();
+                        //User port selection
+                        System.out.println("COM Ports available on machine");
 
-                 SerialPort ports[] = SerialPort.getCommPorts();
-                int i = 1;
-                //User port selection
-                System.out.println("COM Ports available on machine");
+                        for (SerialPort port : ports) {
+                                      int i = 1;                                               //iterateur afin de passer dans le tableau des ports
+                                    System.out.println(i++ + ": " + port.getSystemPortName()); //print windows com ports
+                        }
 
-                for (SerialPort port : ports) {
-                //iterator to pass through port array
-                System.out.println(i++ + ": " + port.getSystemPortName()); //print windows com ports
-                }
+                        try {
+                                comPort.openPort();
+                                comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+                               // comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
+                                //for (int j = 0; j < 1000; ++j)
+                                 //   System.out.print((char)in.read());
+                                //in.close();
 
-        try
-                {
-                    comPort.openPort();
-                   // comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 100, 0);
-                    //for (int j = 0; j < 1000; ++j)
-                     //   System.out.print((char)in.read());
-                    //in.close();
+                                while (true)
+                                        {
+                                            while (comPort.bytesAvailable() == 0)
+                                                Thread.sleep(200);
 
-                    while (true)
-                    {
-                        while (comPort.bytesAvailable() == 0)
-                            Thread.sleep(200);
+                                            byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                                            //byte[] readBuffer = new byte[30];
+                                            int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                                            //System.out.println("Read " + numRead + " bytes.");
+                                            //System.out.println("Read " + readBuffer + " bytes.");
+                                            String s = new String(readBuffer);
+                                            Scanner T = new Scanner(s);
 
-                        byte[] readBuffer = new byte[comPort.bytesAvailable()];
-                        int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                        //System.out.println("Read " + numRead + " bytes.");
-                        //System.out.println("Read " + readBuffer + " bytes.");
-                        String s = new String(readBuffer);
-                        System.out.println(s);
-                    }
+                                            //String yourPattern= "(.*)(\\d+)(.*)";
+                                            String yourPattern= "(.*)(\\b)(.*)";
 
-                }
+                                            String nextMatch = T.findWithinHorizon(yourPattern,0);
+
+                                            TimeUnit.SECONDS.sleep(1); // METTRE une valeur qui est en opposition de phase avec les envois de l'arduino
+
+                                          if(nextMatch.contains("Temperature = ")){
+
+                                              System.out.println("TTTTTTTTTTTTT");
+                                              System.out.println("regex1 :"+ nextMatch);
+                                              Pattern p = Pattern.compile("[-+]?([0-9]*\\.[0-9]+|[0-9]+)");
+                                              TimeUnit.SECONDS.sleep(2);
+                                              Matcher m = p.matcher(nextMatch);
+                                              if( m.find() ){
+                                                  //System.out.println( "-->>" + m.group() );
+                                                  float f = Float.parseFloat(m.group());
+                                                  System.out.println(f);
+                                                  System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+                                                  System.out.println("\n");
+                                              }
+                                                } else if (nextMatch.contains("Humidity = ")){
+
+                                                System.out.println("HHHHHHHHHHHHHHHHHHHH");
+                                              System.out.println("regex2 :"+ nextMatch);
+                                              Pattern p = Pattern.compile("[-+]?([0-9]*\\.[0-9]+|[0-9]+)");
+                                              TimeUnit.SECONDS.sleep(2);
+                                              Matcher m = p.matcher(nextMatch);
+                                              if( m.find() ){
+                                                  //  System.out.println( "-->>" + m.group() );
+                                                  float f = Float.parseFloat(m.group());
+                                                  System.out.println(f);
+                                                  System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                                  System.out.println("\n");
+                                              }
+                                            }
+
+                                        }
+
+                            }
                     catch (Exception e){ e.printStackTrace(); comPort.closePort(); }
-            }
-            public void TEST(/*String[] args*/)throws IOException, InterruptedException {
-                SerialPort sp = SerialPort.getCommPort("/dev/ttyACM0"); // device name TODO: must be changed
-                sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
-                sp.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
-
-                if (sp.openPort()) {
-                    System.out.println("Port is open :)");
-                } else {
-                    System.out.println("Failed to open port :(");
-                    return;
-                }
-
-                /*for (Integer i = 0; i < 5; ++i) {
-                    sp.getOutputStream().write(i.byteValue());
-                    sp.getOutputStream().flush();
-                    System.out.println("Sent number: " + i);
-                    Thread.sleep(1000);
-                }
-*/
-                if (sp.closePort()) {
-                    System.out.println("Port is closed :)");
-                } else {
-                    System.out.println("Failed to close port :(");
-                    return;
-                }
-
-            }
+                          }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
         Controller COntro = new Controller();
         COntro.test();
         //COntro.TEST();
